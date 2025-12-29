8282bff582eed239bedb2354beef71b983be481b
Christian de Jesus Gomez Murillo <cjgm14@gmail.com>
2025-12-28 22:19:47 -0600

    feat: montados los componentes base montados y tokenizacion de colores

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
+
   return (
     <div>
-        <h1 className="text-3xl underline">Hola Mundo!</h1>
+        <h1 className="text-3xl underline mb-3">HOME PAGE</h1>
     </div>
   );
 }
